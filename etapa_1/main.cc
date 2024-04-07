0a81b916b657107c020d1ebd88dcfd5631de9b58
Andrés Víquez Marchena <ANDRES.VIQUEZMARCHENA@UCR.AC.CR>
2024-04-07 12:30:22 -0600

    Proceso de la figura completa
    ToDo: Mejorar la estética del resultado

--- etapa_1/main.cc
+++ etapa_1/main.cc
@@ -33,55 +33,60 @@
 std::string castHTML(const char* htmlResponse) {
   std::string htmlString(htmlResponse);
   std::regex tagRegex("\\s{2,}|<[^>]+>|\\n"); // Regular expression to match HTML tags
-  // std::regex tagRegex("(CI-0123 Proyecto integrador redes y sistemas operativos \\(PIRO\\)|<TD ALIGN=center> \\d|Descripción|Cantidad|\\/|Regresar|Imagen|[\\{\\}]+|window\\.location\\( \"\\/lego\\/index\\.php\" \\);|function home\\(\\) {|\\s{2,}|<[^>]+>)");
   std::string fixedHTML = std::regex_replace(htmlString, tagRegex, ""); // Replaces <div> ..... </diV with ""
   fixedHTML.erase(0, 391);
-  
+  // fixedHTML = std::regex_replace(fixedHTML, std::regex("  "), "\n"); // Replaces "  " with "\n"
   return fixedHTML;
 }
 
 int main(int argc, char* argv[]) {
-  const char* osv4 = "163.178.104.187";
-
-  VSocket* client;
-  VSocket* client2;
-  char bufferArray[100000];
-  char bufferArray2[100000];
-  std::string animal;
-  std::string request1 = "";
-  std::string request2 = "";
-  memset(bufferArray, 0, 100000);
-  memset(bufferArray2, 0, 100000);
-
+  
   if (argc > 1) {
+    const char* osv4 = "163.178.104.187";
+    VSocket* client;
+    VSocket* client2;
+    char bufferArray[100000];
+    char bufferArray2[100000];
+    std::string animal;
+    std::string request = "";
+
     // Get the animal and legoPart from command line arguments
     animal = argv[1];
-    
     // Construct the HTTP request using std::stringstream
     std::stringstream requestStream;
-    std::stringstream requestStream2;
-    requestStream << "GET /lego/list.php?figure=" << animal << "&part=1 HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
-    request1 = requestStream.str();
-    
-    // Create an SSL socket and connect to the server
-    client = new SSLSocket();			// Create an IPv4 TCP SSL socket
-    client->Connect(osv4, 443);
-    client->Write(request1.c_str());
-    client->Read(bufferArray, 100000);
-    client->Write(request1.c_str());
-    client->Read(bufferArray, 100000);
-    std::string htmlResponse = castHTML(bufferArray);
-    
-    if (htmlResponse.empty()) {
-      std::cout << RED << "\n\tError 404! Figure NOT found\n" << RESET << std::endl;
-    } else if (htmlResponse.find("Illegal request") != std::string::npos) {
-      std::cout << RED << "\n\tError 404! Illegal request\n" << RESET << std::endl;
-    } else {
-      std::transform(animal.begin(), animal.end(), animal.begin(), ::toupper);
-      std::cout << CYAN << "\n\tLEGO PIECES FOR " << animal << "\n" << RESET << htmlResponse << std::endl << std::endl;
-    }
-    delete client; // Free the memory allocated for the socket
 
+    std::transform(animal.begin(), animal.end(), animal.begin(), ::toupper);
+    std::cout << CYAN << "\n\tLEGO PIECES FOR " << animal << "\n" << RESET;
+    std::transform(animal.begin(), animal.end(), animal.begin(), ::tolower);
+
+    for (int requestNumber = 0; requestNumber < 2; requestNumber++) {
+      // Create an SSL socket and connect to the server
+      client = new SSLSocket();			// Create an IPv4 TCP SSL socket
+      client->Connect(osv4, 443); 
+
+      memset(bufferArray, 0, 100000);
+      requestStream.str("");
+      requestStream.clear();
+      requestStream << "GET /lego/list.php?figure=" << animal << "&part=" << (requestNumber + 1) <<  " HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+      request = requestStream.str();
+
+      client->Write(request.c_str());
+      client->Read(bufferArray, 100000);
+      client->Write(request.c_str());
+      client->Read(bufferArray, 100000);
+      std::string htmlResponse = castHTML(bufferArray);
+
+      if (htmlResponse.empty()) {
+        std::cout << RED << "\n\tError 404! Figure NOT found\n" << RESET << std::endl;
+      } else if (htmlResponse.find("Illegal request") != std::string::npos) {
+        std::cout << RED << "\n\tError 404! Illegal request\n" << RESET << std::endl;
+      } else {
+        //std::cout << htmlResponse << std::endl << std::endl;
+        std::cout << bufferArray << std::endl << std::endl;
+      }
+      delete client; // Free the memory allocated for the socket
+    }
+  
   } else {
     std::cout << RED << "\n\tUsage: ./main.out <animal> \n" << RESET << std::endl;
   }
