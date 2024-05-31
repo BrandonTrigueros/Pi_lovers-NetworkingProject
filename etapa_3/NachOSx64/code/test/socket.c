60a3f1ed4d029bb531580fe55f4dd2bac415ae4a
JOSE CARLOS MENA DIAZ <jose.menadiaz@ucr.ac.cr>
2024-05-30 22:19:02 -0600

    Switch config

--- etapa_3/NachOSx64/code/test/socket.c
+++ etapa_3/NachOSx64/code/test/socket.c
@@ -2,63 +2,9 @@
 
 int main() {
   int id;
-  char buffer1[512];
-  char* request1 = (char*)"NachOS legoFigures/get-BobOmb";
+  char buffer[512];
+  char* request = (char*)"NachOS legoFigures/get-BobOmb";
 
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer2[512];
-  char* request2 = (char*)"NachOS legoFigures/get-Dragon";
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer3[512];
-  char* request3 = (char*)"NachOS legoFigures/get-Car";
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer4[512];
-  char* request4 = (char*)"NachOS legoFigures/get-DogHouse";
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer4[512];
-  char* request4 = (char*)"NachOS legoFigures/get-DogHouse";
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer5[512];
-  char* request5 = (char*)"NachOS legoFigures/get-TieFighter";
-  id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
-  Connect(id, "127.0.0.0", 1233);
-  Write(request, 512, id);
-  Read(buffer, 512, id);
-  Write(buffer, 512, 1);
-  Close(id);
-
-  char buffer6[512];
-  char* request6 = (char*)"NachOS legoFigures/get-Cat";
   id = Socket(AF_INET_NachOS, SOCK_STREAM_NachOS);
   Connect(id, "127.0.0.0", 1233);
   Write(request, 512, id);
