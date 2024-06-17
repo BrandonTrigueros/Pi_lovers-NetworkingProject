b258ee1eb67caa240eb4bfb8de96dedef74bad18
Andrés Víquez Marchena <ANDRES.VIQUEZMARCHENA@UCR.AC.CR>
2024-06-17 17:22:03 -0600

    REQUEST QUEUE enqueue method added

--- etapa_4/Intermedio/RequestQueue.cpp
+++ etapa_4/Intermedio/RequestQueue.cpp
@@ -1,29 +1,35 @@
 #include "RequestQueue.h"
 
 RequestQueue::RequestQueue() {
-
 }
 
 RequestQueue::~RequestQueue() {
-
 }
 
 void RequestQueue::initQueue(RequestQueue* queue) {
-
+  queue->head = nullptr;
+  queue->tail = nullptr;
 }
 
 void RequestQueue::destroyQueue(RequestQueue* queue) {
-
+  queue->clearQueue(queue);
 }
 
 bool RequestQueue::isEmpty(RequestQueue* queue) {
-
+  return queue->head == NULL;
 }
 
-
-
 void RequestQueue::enqueue(RequestQueue* queue, char* request) {
-
+  queue_node* new_node = (queue_node*) calloc(sizeof(queue_node), 1);
+  if (new_node) {
+    new_node->request = request;
+    if (queue->tail) {
+      queue->tail = queue->tail->next_request = new_node;
+    } else {
+      queue->head = queue->tail = new_node;
+    }
+    std::cout << "Request enqueued: " << request << std::endl;
+  }
 }
 
 char* RequestQueue::dequeue(RequestQueue* queue, char* request) {
