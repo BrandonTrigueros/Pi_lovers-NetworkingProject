433b4c256a160175a252dd6aaf528206aa49c215
Fabian <josefabian.guzman@ucr.ac.cr>
2024-06-16 20:30:44 -0600

    Client constructor, printResponse, analyzeArgs

--- etapa_4/Cliente/Client.cc
+++ etapa_4/Cliente/Client.cc
@@ -2,7 +2,12 @@
 #include "Client.h"
 
 Client::Client() {
-
+  this->clientSocket = nullptr;
+  this->socketType = ' ';
+  this->figure = "";
+  this->request = "";
+  this->ipDirection = nullptr;
+  memset(this->buffer, 0, MAXBUF);
 }
 
 Client::~Client() {
@@ -14,9 +19,21 @@ void Client::run() {
 }
 
 bool Client::analyzeArgs(int argc, char *argv[]) {
-
+  bool validArgs = false; // Assumes the arguments are invalid
+  if (argc > 1)
+  {
+    validArgs = true;
+    this->figure = argv[1];
+  }
+  else
+  {
+    std::cerr << RED << "WRONG USAGE!\n"
+              << RESET << "Usage: ./Client.out <legoFigure>" << std::endl;
+  }
+  return validArgs;
 }
 
 void Client::printResponse() {
-
+  std::cout << "\t" << CYAN << this->figure << " parts" << RESET << std::endl;
+  std::cout << this->response << std::endl;
 }
