949b46e9791fa5aabd1eb86b7311bcd15c40686f
Andrés Víquez Marchena <ANDRES.VIQUEZMARCHENA@UCR.AC.CR>
2024-06-16 20:34:33 -0600

    Intermediate server definition

--- etapa_4/IntermedioMaster/MasterIntermediate.h
+++ etapa_4/IntermedioMaster/MasterIntermediate.h
@@ -1,9 +1,44 @@
 #ifndef MASTERINTERMEDIATE_H
 #define MASTERINTERMEDIATE_H
 
+#include <thread>
+#include <iostream>
+#include "Socket.h"
+#include "VSocket.h"
+
+#define CLIENT_PORT 1234 //4100
+#define TCP_PORT 4200
+#define UDP_PORT 4300
+#define BUFFER_SIZE 512
+
 class MasterIntermediate {
   private:
- 
+  char* userRequest;
+  // Request queue
+  // Response queue
+  // Map intermediates & ip
+  // Multimap Figuras & servers
+
   public:
+  MasterIntermediate();
+  ~MasterIntermediate();
+  void run();
+
+  void clientIntermediate();
+  void listenClient();
+  static void handleClient(void*);
+  // void sendTCP();
+  // void receiveTCP();
+  
+  // void sendUDP();
+  // void receiveUDP();
+
+  // buildRequest();
+  // removeFromQueue();
+
+  // addToQueue();
+
+  // castHTML();
+  
 };
 #endif // MASTERINTERMEDIATE_H
