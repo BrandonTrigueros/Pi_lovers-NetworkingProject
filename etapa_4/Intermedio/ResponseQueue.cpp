7ee345cbf6291681e2fd655860de27edb503e13e
BrandonTrigueros <brandon.trigueros@ucr.ac.cr>
2024-06-17 17:36:47 -0600

    metodos de la cola de respuestas

--- etapa_4/Intermedio/ResponseQueue.cpp
+++ etapa_4/Intermedio/ResponseQueue.cpp
@@ -2,15 +2,37 @@
 
 ResponseQueue::ResponseQueue() { }
 
-ResponseQueue::~ResponseQueue() { }
+ResponseQueue::~ResponseQueue() {
+  // destroyQueue();
+}
 
-void ResponseQueue::initQueue(ResponseQueue* queue) { }
+void ResponseQueue::initQueue(ResponseQueue* queue) {
+  queue->head = nullptr;
+  queue->tail = nullptr;
+}
 
-void ResponseQueue::destroyQueue(ResponseQueue* queue) { }
+void ResponseQueue::destroyQueue(ResponseQueue* queue) {
+  queue->clearQueue(queue);
+}
 
-bool ResponseQueue::isEmpty(ResponseQueue* queue) { }
+bool ResponseQueue::isEmpty(ResponseQueue* queue) {
+  return queue->head == NULL;
+}
 
-void ResponseQueue::enqueue(ResponseQueue* queue, char* request) { }
+void ResponseQueue::enqueue(ResponseQueue* queue, char* request) {
+  queue_node* new_node = (queue_node*)calloc(sizeof(queue_node), 1);
+
+  if (new_node) {
+    new_node->request = request;
+    if (queue->tail) {
+      queue->tail = queue->tail->next_request = new_node;
+    } else {
+      queue->head = queue->tail = new_node;
+    }
+
+    std::cout << "Request enqueued: " << request << std::endl;
+  }
+}
 
 char* ResponseQueue::dequeue(ResponseQueue* queue, char* request) { }
 
