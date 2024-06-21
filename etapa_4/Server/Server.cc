323761596301308a35b67aa9beb3f97fe50f8c92
jose.menadiaz <jose.menadiaz@ucr.ac.cr>
2024-06-20 18:13:56 -0600

    Escaneo de HTMLs

--- etapa_4/Server/Server.cc
+++ etapa_4/Server/Server.cc
@@ -1,25 +1,43 @@
 #include "Server.h"
 
-void Server::run()
-{
-  std::thread *worker;
-  worker = new std::thread(listenIntermediateUDP);
-  worker->join();
+void Server::run() {
+  // std::thread* worker;
+  // worker = new std::thread(listenIntermediateUDP);
+  // worker->join();
+
+  // scanExistingPieces();
+  this->scanExistingPieces();
+  // print this->serverPieces
+  for (const auto& piece : this->serverPieces) {
+    std::cout << piece << std::endl;
+  }
 }
 
 void Server::listenIntermediateUDP() {
   struct sockaddr serverInfo;
-  VSocket *intermediate = new Socket('d', false);
+  VSocket* intermediate = new Socket('d', false);
   intermediate->Bind(UDP_PORT);
   char buffer[BUFFER_SIZE];
-  char* message = (char *) "Connection accepted";
+  char* message = (char*)"Connection accepted";
   memset(&serverInfo, 0, sizeof(serverInfo));
   std::cout << "Server is running" << std::endl;
   while (true) {
-    intermediate->recvFrom((void*) buffer, BUFFER_SIZE, (void*)&serverInfo);
+    intermediate->recvFrom((void*)buffer, BUFFER_SIZE, (void*)&serverInfo);
     std::cout << "Received data: " << buffer << std::endl;
-    intermediate->sendTo((void*) message, strlen(message), (void*)&serverInfo);
+    intermediate->sendTo((void*)message, strlen(message), (void*)&serverInfo);
     std::cout << "Sent data: " << message << std::endl;
   }
   intermediate->Close();
+}
+
+void Server::scanExistingPieces() {
+  std::string directoryPath = "./Legos";
+  if (!std::filesystem::exists(directoryPath)) {
+    std::cerr << "El directorio no existe: " << directoryPath << std::endl;
+  }
+  for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
+    if (entry.is_regular_file() && entry.path().extension() == ".html") {
+      this->serverPieces.push_back(entry.path().filename().string());
+    }
+  }
 }
